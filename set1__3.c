474ae15aa024a52317bbe289846013d0c8e53917
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-02-23 01:39:01 -0800

    Create set1__3.c

---
+++ set1__3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+  char a;
+  scanf("%c",&a);
+  switch(a)
+    {
+      case 'a':
+      printf("IS A VOWEL");
+      break;
+      case 'e':
+      printf("IS A VOWEL");
+      break;
+      case 'i':
+      printf("IS A VOWEL");
+      break;
+      case 'o':
+      printf("IS A VOWEL");
+      break;
+      case 'u':
+      printf("IS A VOWEL");
+      break;
+      default:
+      printf("CONSONANT");
+    }
+    return 0;
+}
